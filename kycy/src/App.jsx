e90fbb60ce87b73b62940c49f0ad509215e43de4
victoras24 <fin17217@uom.edu.gr>
2024-03-10 14:28:27 +0200

    replaced browserouter with hashrouter

--- kycy/src/App.jsx
+++ kycy/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Layout from "./components/Layout"
 import Search from "./pages/Search"
 import './styles/index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, HashRouter } from "react-router-dom"
 
 export default function App() {
     return (
-        <BrowserRouter >
+        <HashRouter >
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Search />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
